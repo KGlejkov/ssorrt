9ab410b91fa7096f3b7be215d211f6d597ec951e
KGlejkov <gklejkov@gmail.com>
2024-11-25 08:54:53 +0700

    Update timeS.py

--- timeS.py
+++ timeS.py
@@ -4,17 +4,17 @@ import random
 
 a=[10,100,1000,10000,10**7]
 mas=[]
-for n in a:
+for j in a:
     mas.append([random.randint(-100,100) for _ in range(n)])
     print(n)
-start=time.time()
+st=time.time()
 print(start)
-#sort.bubble_sort(mas[1])
-#sort.select_sort(mas[3])
-#sort.insertion_sort(mas[3])
-sort.quick_sort(mas[4],0,len(mas[4])-1)
-#sort.cocktailSort(mas[3])
+#sort.bubblesort(mas[1])
+#sort.selectsort(mas[3])
+#sort.insertionsort(mas[3])
+sort.quicksort(mas[4],0,len(mas[4])-1)
+#sort.cocktailsort(mas[3])
 end=time.time()
 print(end)
-T=end - start
-print(T)
+t0=end-st
+print(t0)
